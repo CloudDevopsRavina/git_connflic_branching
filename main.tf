471b75fd9884b623638db2237dda5d000615a9fe
Ravina Rathod <ravinarathod009@gmail.com>
2026-03-31 23:38:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ ec2 = boto3.client('ec2')
 
 def lambda_handler(event, context):
     response = ec2.run_instances(
-        ImageId='ami-0ff5003538b60d5ec',  # replace with valid AMI
+        ImageId='ami-',  # replace with valid AMI
         InstanceType='t3.micro',
         MinCount=1,
         MaxCount=1,
